502830e519838ec10c4ddbe250f05fc6551b6efa
izzat <izzatfadzlon@gmail.com>
2025-03-09 14:46:00 +0800

    interest-rule: generated daily balance dictionary to be used for populating date_balance_rule_array

--- classes/account.py
+++ classes/account.py
@@ -70,21 +70,45 @@ class Account:
     def generate_monthly_statement(self, year, month, interest_rules):
         statement = f"Account: {self.account} \n"
         statement += "| Date         | Txn Id           | Type | Amount | Balance | \n"
-        balance = self.get_balance_before_date(year, month)
+        initial_balance = self.get_balance_before_date(year, month)
 
         # get the transactions in requested month
         monthly_transactions = self.get_transactions_in_month(year, month)
 
+        current_balance = initial_balance
         for txn in monthly_transactions:
             if txn.type == "D":
-                balance += txn.amount
+                current_balance += txn.amount
             else:
-                balance -= txn.amount
-            statement += f"| {txn.date}     | {txn.txn_id}      | {txn.type}    | {txn.amount}  | {balance} | \n"
+                current_balance -= txn.amount
+            statement += f"| {txn.date}     | {txn.txn_id}      | {txn.type}    | {txn.amount}  | {current_balance} | \n"
 
         # --- CALCULATE INTEREST ---
         # input: interest_rules[]:[Interest:{date, ruleId, rate}][], monthly_transactions[]:[Transaction:{account,date,txn_id,type,amount}][]
-        
+        daily_balance_dict = defaultdict(int)
+        last_day = calendar.monthrange(year, month)[1]
+
+        current_balance_2 = initial_balance
+        # populate the daily_balance_dict dictionary {[key:date_str]:balance}
+        for day in range(1, last_day + 1):
+            date_str = f"{year}{month:02}{day:02}"
+            # filter transaction in current day
+            day_transactions = [
+                txn for txn in monthly_transactions if txn.date == date_str
+            ]
+            # get current balance given multiple txn in a day
+            for txn in day_transactions:
+                if txn.txn_id == "D":
+                    current_balance_2 += txn.amount
+                else:
+                    current_balance_2 -= txn.amount
+            daily_balance_dict[txn.date] = current_balance_2
+
+        # array to hold all dates balance rule within the month
+        date_balance_rule_array = []
+        for day in range(1, last_day + 1):
+            date_str = f"{year}{month:02}{day:02}"
+
         return statement
 
     def get_balance_before_date(self, year, month):
@@ -97,7 +121,7 @@ class Account:
                 else:
                     balance -= txn.amount
         return balance
-    
+
     def get_transactions_in_month(self, year, month):
         start_date = f"{year}{month:02}01"
         last_day = calendar.monthrange(year, month)[1]
