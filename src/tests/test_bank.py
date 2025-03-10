8e349925c97b7d235d02a4c6172bab690520dc66
izzat <izzatfadzlon@gmail.com>
2025-03-10 17:49:28 +0800

    initialize working test structure

---
+++ src/tests/test_bank.py
@@ -0,0 +1,18 @@
+import pytest
+from classes.bank import Bank
+
+
+def test_add_transaction():
+    bank = Bank()
+
+
+def test_insufficient_balance():
+    bank = Bank()
+
+
+def test_interest_rule_replacement():
+    bank = Bank()
+
+
+def test_statement_generation():
+    bank = Bank()
